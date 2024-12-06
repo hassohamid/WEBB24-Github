726d0eb507478f037e83c77b15b40e83ca2332a1
hassohamid <hassohamid@hotmail.com>
2024-12-06 13:06:43 +0100

    UserList added

--- src/pages/Userlist.jsx
+++ src/pages/Userlist.jsx
@@ -1,7 +1,37 @@
-let Userlist = () => {
-    return (
-        <>
-        </>
-    )
+import { useState } from "react";
+
+export default function Userlist() {
+  const [users, setUsers] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+
+  const addUser = () => {
+    const newUser = inputValue;
+    setUsers([...users, newUser]);
+    setInputValue("");
+  };
+
+  const removeUser = (index) => {
+    setUsers(users.filter((_, i) => i !== index));
+  };
+
+  return (
+    <>
+      <h1> Userlist: </h1>
+      <input
+        type="text"
+        value={inputValue}
+        placeholder="Username"
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <button onClick={addUser}> Add New User </button>
+      <ul>
+        {users.map((user, index) => (
+          <li key={index}>
+            {user}{" "}
+            <button onClick={() => removeUser(index)}> Delete User </button>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
-export default Userlist;
